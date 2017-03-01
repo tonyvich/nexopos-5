9ec58071764fd64407579c865211d77879d74ad9
Blair Jersyer <carlosjohnsonluv2004@gmail.com>
2017-03-01 17:50:50 +0100

    Liste des options à créer
    Liste des options à créer

---
+++ options-details.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Ce fichier contient l'ensemble des options disponible dans NexoPOS
+ *
+ * //  Section Generale
+ *
+ * shop_name :
+ * Contient le nom du site
+ *
+ * shop_phone :
+ * Contient le numéro de téléphone de la boutique
+ *
+ * shop_fax :
+ * Contient le numéro de fax de la boutique
+ *
+ * shop_email :
+ * Contient l'email de la boutique
+ *
+ * shop_opening :
+ * contient l'heure d'ouverture de la boutique
+ *
+ * shop_closing :
+ * content l'heure de fermeture de la boutique
+ *
+ * shop_pobox :
+ * contient la boite postale de la boutique
+ *
+ * shop_address :
+ * contient l'adresse de la boutique
+ *
+ * shop_name :
+ * Contient le nom du site
+ *
+ * shop_currency_symbol
+ * contient le symbol de la devise
+ *
+ * shop_currency_iso
+ * content le format ISO de la devise
+ *
+ * // Section Caisse
+ *
+ * shop_checkout_soundfx :
+ * permet d'activer les effets sonores sur la caisse
+ *
+ * shop_checkout_autoprint :
+ * permet d'activer l'impression automatique
+ *
+ * shop_checkout_register :
+ * permet l'utilisation des caisses
+ *
+ * shop_checkout_discount_status :
+ * permet d'activer ou de masquer l'option des remises
+ *
+ * shop_checkout_item_status :
+ * permet d'activer ou de masquer l'édition d'article dans le panier
+ *
+ * shop_checkout_vat_status
+ * permet d'activer les taxes
+ *
+ * shop_checkout_fullscreen_status
+ * permet d'activer le mode plein écran
+ *
+ * shop_checkout_smsinvoice_status
+ * permet d'activer les factures SMS
+ *
+ * shop_checkout_create_customer_status
+ * permet d'activer la création de client
+ *
+ * shop_checkout_coupon_status
+ * permet d'activer les coupons
+ *
+ * shop_checkout_item_discount_status
+ * permet d'activer les remises par articles
+ *
+ * shop_checkout_keyboard_status
+ * permet d'activer les claviers
+ *
+ * shop_checkout_vat_percent
+ * permet de définir le pourcentage des taxes
