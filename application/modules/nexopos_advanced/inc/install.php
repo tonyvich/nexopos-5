c362aeec58e316dfd34dc536169407a355e95983
Tinyskillz <Tinyskillz69@gmail.com>
2017-03-18 11:41:04 +0100

    Updating customers table

--- application/modules/nexopos_advanced/inc/install.php
+++ application/modules/nexopos_advanced/inc/install.php
@@ -434,17 +434,18 @@ class NexoPOS_Install extends Tendoo_Module
 
     $this->db->query('CREATE TABLE IF NOT EXISTS `'.$table_prefix.'nexopos_customers_address` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
-      `company` varchar(200) NOT NULL,
-      `first_address` varchar(200) NOT NULL,
-      `second_address` varchar(200) NOT NULL,
-      `pobox` varchar(200) NOT NULL,
-      `author` int(11) NOT NULL,
-      `town` varchar(200) NOT NULL,
-      `country` varchar(200) NOT NULL,
-      `state` varchar(200) NOT NULL,
-      `ref_customer` varchar(200) NOT NULL,
-      `date_creation` datetime NOT NULL,
-      `date_modification` datetime NOT NULL,
+      `company` varchar(200),
+      `first_address` varchar(200),
+      `second_address` varchar(200),
+      `pobox` varchar(200),
+      `author` int(11),
+      `town` varchar(200),
+      `country` varchar(200),
+      `state` varchar(200),
+      `ref_customer` varchar(200),
+      `type` varchar(200),
+      `date_creation` datetime,
+      `date_modification` datetime,
       PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
 
