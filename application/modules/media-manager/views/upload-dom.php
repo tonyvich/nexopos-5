c258178c1101f8a946ea1b4681efe6b465bac11f
Tyniskillz69 <Tyniskillz69@gmail.com>
2017-05-03 13:55:16 +0100

    Amélioration vue media manager

--- application/modules/media-manager/views/upload-dom.php
+++ application/modules/media-manager/views/upload-dom.php
@@ -9,7 +9,7 @@
             <div class="tab-content">
               <div class="tab-pane active" id="main_page">
                   <div class="row">
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                           <div class="row">
                               <div class="col-md-12">
                                   <div class="input-group input-group-lg">
@@ -24,33 +24,39 @@
                                   </div>
                                   <br>
                               </div>
-                              <div class="md-col-12">
-                                  <div class="container-fluid" style="padding-top:5px;overflow-y:scroll;height:300px;">
-                                      <div
-                                        ng-click="selectEntry( entry, index )"
-                                        ng-class="{ 'selected' : entry.selected }"
-                                        class="media-manager-entry-box"
-                                        ng-repeat="(index, entry) in entries">
-                                          <img ng-src="{{ entry.thumb }}"/>
-                                      </div>
+                              <div class="col-md-12">
+                                  <div class="col-md-{{ mainMediaDivHeight }}">
+                                    <div class="container-fluid" style="padding-top:5px;overflow-y:scroll;height:300px;">
+                                        <div
+                                            ng-click="selectEntry( entry, index )"
+                                            ng-class="{ 'selected' : entry.selected }"
+                                            class="media-manager-entry-box"
+                                            ng-repeat="(index, entry) in entries">
+                                            <img ng-src="{{ entry.thumb }}"/>
+                                        </div>
+                                    </div>
                                   </div>
+                                  <div class="col-md-3" ng-show="showDetails == true">
+                                    <div ng-show="countSelected() == 1">
+                                        <h2 class="page-header">
+                                            <i class="fa fa-globe"></i> <?php echo __( 'Details', 'media-manager' );?>
+                                        </h2>
+                                            <p>
+                                                <strong><?php echo __( 'Name' , 'media-manager' );?> : </strong>
+                                                {{ entries[ selectedIndex ].name }}
+                                            </p>
+                                            <p>
+                                                <strong><?php echo __( 'Uploaded' , 'media-manager' );?> : </strong>
+                                                {{ entries[ selectedIndex ].date_creation }}
+                                            </p>
+                                    </div>
+                                    <div ng-show="countSelected() > 1">
+                                        <h2 class="page-header">
+                                            <i class="fa fa-globe"></i> <?php echo __( 'Details', 'media-manager' );?>
+                                        </h2>
+                                    </div>
+                                </div>
                               </div>
-                          </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div ng-show="countSelected() == 1">
-                              <h2 class="page-header">
-                                <i class="fa fa-globe"></i> <?php echo __( 'Details', 'media-manager' );?>
-                              </h2>
-                                <p>
-                                    <strong><?php echo __( 'Name' , 'media-manager' );?> : </strong>
-                                    {{ entries[ selectedIndex ].name }}
-                                </p>
-                          </div>
-                          <div ng-show="countSelected() > 1">
-                              <h2 class="page-header">
-                                <i class="fa fa-globe"></i> <?php echo __( 'Details', 'media-manager' );?>
-                              </h2>
                           </div>
                       </div>
                   </div>
