2f4a62af287b517fcf175ab25466c1019ad466ff
Blair Jersyer <Blair2004@users.noreply.github.com>
2017-05-10 10:55:04 +0100

    Update directive.php

--- application/modules/user_log/views/directive.php
+++ application/modules/user_log/views/directive.php
@@ -32,7 +32,6 @@
                     $scope.timerIncrement = function (){
                         $scope.idleTime = $scope.idleTime + 1;
                         if ($scope.idleTime > 1) { // faisons un test sur 1 minute
-                            // pourquoi ne pas utiliser le sign-out déjà existant [ 'sign-out' ] ?
                             window.location.assign("<?php echo site_url( [ 'dashboard', 'user_log', 'outer' ] ); ?>");
                             return;
                         }
