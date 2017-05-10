67f305bda63ff419acf7aaa41a87974e1dbc7b8a
Tyniskillz69 <Tyniskillz69@gmail.com>
2017-05-10 01:52:11 +0100

    Mise a jour user log

--- application/modules/user_log/inc/controller.php
+++ application/modules/user_log/inc/controller.php
@@ -8,6 +8,12 @@ class UserLogController extends Tendoo_Module
         parent::__construct();
     }
 
+    /**
+     *  Log session  (log users session)
+     *  @param
+     *  @return
+    **/
+
     public function log_session(){
         $ip        = $this->input->ip_address();
         $user_id   = User::id();
@@ -53,6 +59,43 @@ class UserLogController extends Tendoo_Module
                 "closed"           =>      "no"
             ));
         }
+    }
+
+    /**
+     *  outer (Disconnect Iddle users)
+     *  @param
+     *  @return
+    **/
+
+    public function outer()
+    {
+        $ip        = $this->input->ip_address();
+        $user_id   = User::id();
+
+        // Closing sessions 
+        $this->db->select("
+            id,
+            user,
+            IP_address,
+            date_connexion,
+            date_deconnexion,
+            closed,
+            TIMEDIFF(date_deconnexion, date_connexion) as duree
+        ");
+
+        $this->db->where("TIMESTAMPDIFF(MINUTE, date_connexion, date_deconnexion) > 5");
+        $query = $this->db->get("user_log_sessions");
+        if( $query->num_rows() > 0 ){
+            foreach( $query->result() as $row ){
+                $this->db->where("id",$row->id);
+                $this->db->update("user_log_sessions",array("closed" => "yes", "duree_session" => $row->duree));
+            }
+        }
+        
+        // Loading libraries
+        $this->load->library('aauth',  array(),  'auth');
+        $this->auth->logout();
+        redirect(array( 'sign-in?redirect=dashboard/'));
     }
     
 }
--- application/modules/user_log/views/directive.php
+++ application/modules/user_log/views/directive.php
@@ -8,12 +8,36 @@
             controller : [
                 '$scope',
                 '$http',
-                function( $scope, $http ){
+                '$element',
+                function( $scope, $http, $element){
                     $scope.logSession = function(){
                         $http.get( "<?php echo site_url( [ 'dashboard', 'user_log', 'log_session' ] ); ?>");
                     }
 
-                    setInterval($scope.logSession, 10000);                   
+                    setInterval($scope.logSession, 10000);
+
+                    // Handling idle user
+
+                    $scope.idleTime = 0;
+                    var body = angular.element("body");
+                    
+                    body.mousemove(function(e){
+                        $scope.idleTime = 0;
+                    });
+
+                    body.keypress(function (e) {
+                        $scope.idleTime = 0;
+                    });
+
+                    $scope.timerIncrement = function (){
+                        $scope.idleTime = $scope.idleTime + 1;
+                        if ($scope.idleTime > 10) { // 10 minutes
+                            window.location.assign("<?php echo site_url( [ 'dashboard', 'user_log', 'outer' ] ); ?>");
+                            return;
+                        }
+                    }
+
+                    setInterval($scope.timerIncrement, 60000); // 1 minute                   
                 }
             ]
         }
